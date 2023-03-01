1f2376c02c08d77934b52e5198977139e5d84ef3
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-03-01 11:39:09 -0700

    got the buttons values to display on calculator screen. I am trying to figure out how I want to save the equation.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
 console.log(`Calculator`)
 
-const screen = document.getElementById(`displayScreen`)
+const screen = document.getElementById(`displayScreen`);
+const buttons = document.querySelectorAll(`button`);
+
+let calculated
+
+buttons.forEach(button => button.addEventListener(`click`, (e) => {
+    const value = button.textContent;
+
+    screen.innerHTML = value;
+    console.log(value)
+}));
+
+console.log(5 + 5)
