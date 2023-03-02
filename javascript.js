ac820d6bb47c874a4b6072b7c645567a8fd66e68
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-03-02 16:55:56 -0700

    restarted the project from scratch. I think I have a simplar way of getting the input. not sure how I want to try to get the total, but I think think about it.

--- javascript.js
+++ javascript.js
@@ -3,65 +3,25 @@ console.log(`Calculator`)
 const screen = document.getElementById(`displayScreen`);
 const buttons = document.querySelectorAll(`button`);
 
-let calculated = [];
-let equation
+let equation = '';
+let total
 
-buttons.forEach(button => button.addEventListener(`click`, (e) => {
-    
-    const value = button.textContent;
+buttons.forEach(button => button.addEventListener('click', (e) => {
+    const input = button.textContent;
 
-    if (value === 'CLEAR') {
-        calculated = [];
-        screen.textContent = ``
-    } else if (value === '=') {
-        let answer;
-        let operator;
-        for (let val of calculated) {
-            // is this an operator or a number?
-            // if number, make it an int
-            isNumber = (
-                val !== '+' &&
-                val !== '-' &&
-                val !== '*' &&
-                val !== String.fromCharCode(0xF7)
-            )
-            if (isNumber) {
-                val = parseFloat(val);
-                if (answer === undefined) {
-                    answer = val
-                } else {
-                    switch (operator) {
-                        case '+':
-                            answer += val;
-                            break;
-                        case '-':
-                            answer -= val;
-                            break;
-                        case '*':
-                            answer *= val;
-                            break;
-                        case String.fromCharCode(0xF7):
-                            answer /= val;
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            } else {
-                operator = val;
-            }
-        }
-
-        screen.textContent = answer;
+    if (button.textContent === 'CLEAR') {
+        equation = '';
+        screen.textContent = '';
+    } else if (button.textContent === '=') {
+        equation = 
     } else {
-        calculated.push(value);
-        equation = calculated.join(``);
-        screen.innerHTML = equation;
-        console.log(equation);
+        equation += input;
+        screen.textContent = equation;
+        console.log(equation)
     }
-
     
-}));
+    
+}))
 
 console.log(5 + 5);
 
