1459b1aecc8f73d94aaa20002f0cd99bd4bc373e
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-03-03 17:11:13 -0700

    rewrote everything. I wanted to try check if the value was a symbol, but that didn't work. I it worked when you checked if it was a number.

--- javascript.js
+++ javascript.js
@@ -8,23 +8,52 @@ let total;
 
 buttons.forEach(button => button.addEventListener('click', (e) => {
     const input = button.textContent;
-    const isNumber = true;
 
     if (button.textContent === 'CLEAR') {
         equation = '';
         screen.textContent = '';
     } else if (button.textContent === '=') {
-        equation = '';
-        screen.textContent = 'total = P';
+        // equation = '';
+        // screen.textContent = 'total = P';
 
-        //bucket to put total in
+        //bucket to put answer in
+        //bucket to put operator
         //loop through numbers and operations
 
-        for(let value of equation) {
-            if(value = parseFloat())
-        }
+        let answer;
+        let operator;
 
-
+        for (let value of equation) {
+            const isNumber = (
+                value !== '+' &&
+                value !== '-' &&
+                value !== '*' &&
+                value !== String.fromCharCode(0xF7)
+            ) 
+            if (isNumber) {        
+                value = parseFloat(value); 
+                if (answer === undefined) {
+                    answer = value;
+                } else {
+                    switch (operator) {
+                        case '+':
+                            answer += value;
+                            break;
+                        case '-':
+                            answer -= value;
+                            break;
+                        case '*':
+                            answer *= value;
+                            break;
+                        case String.fromCharCode(0xF7):
+                            answer /= value;
+                    }
+                }
+            } else {
+                operator = value;
+            }
+        } 
+            screen.textContent = answer;
     } else {
         equation += input;
         screen.textContent = equation;
